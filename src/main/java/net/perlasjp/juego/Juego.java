bf663ddb5a4da614d2666aa0ee0d5db2e66a9129
Genesis Recinos <recinosgenesis854@gmail.com>
2025-08-11 10:14:26 -0600

    Agregue cambios a clase juego

--- src/main/java/net/perlasjp/juego/Juego.java
+++ src/main/java/net/perlasjp/juego/Juego.java
@@ -1,12 +1,52 @@
 package net.perlasjp.juego;
 
+import java.util.Scanner;
+
 public class Juego {
 
-    private String Jugador;
-    private String Enemigo;
-    private String Combate;
+    private Jugador jugador;
+    private Enemigo enemigo;
+    private Combate combate;
 
     public void iniciar() {
+
+        Scanner scanner = new Scanner(System.in);
+        combate = new Combate();
+        Historia historia = new Historia();
+
+        System.out.println("¡Bienvenido al juego de aventura!");
+        System.out.print("Ingresa tu nombre:");
+        String nombre = scanner.nextLine();
+        jugador = new Jugador();
+
+        historia.introduccion();
+
+        while (true) {
+            System.out.println("\n¿Que desea hacer?");
+            System.out.println("1. Explorar");
+            System.out.println("2. Ver estado");
+            System.out.println("3. Salir");
+
+            String opcion = scanner.nextLine();
+
+            switch (opcion) {
+                case "1":
+                    Enemigo enemigo = new Enemigo();
+                    combate.iniciar(enemigo, jugador);
+                    break;
+                case "2":
+                    // vamos a agregar otro metodo de jugador
+                    break;
+                case "3":
+                    System.out.println("¡Gracias por jugar!");
+                    return;
+                default:
+                    System.out.println("OPcion no valida");
+
+            }
+
+        }
+
     }
 
 }
