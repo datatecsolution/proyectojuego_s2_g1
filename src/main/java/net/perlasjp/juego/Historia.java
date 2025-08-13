36d7b563c7f66d001d5c0fbd12130d096915cf1f
sadier maradiaga <sadiermaradiaga6>
2025-08-13 10:09:59 -0600

    Agregue funciones a clases

--- src/main/java/net/perlasjp/juego/Historia.java
+++ src/main/java/net/perlasjp/juego/Historia.java
@@ -3,7 +3,8 @@ package net.perlasjp.juego;
 public class Historia {
 
     public void introduccion() {
-        
+    System.out.println("\nEstas en un mundo lleno de peligro y misterios ...");
+    System.out.println("Tu mision: sobrevivir y descubrir los secretos del bosque encantado.");
     }
 
 }
--- src/main/java/net/perlasjp/juego/Main.java
+++ src/main/java/net/perlasjp/juego/Main.java
@@ -3,6 +3,6 @@ package net.perlasjp.juego;
 public class Main {
 
     public static void main(String[] agrs) {
-
-    }
+    Juego juego =new Juego();
+    Juego.iniciar();  }
 }
