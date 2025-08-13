0d72b904831295884f430cfe7eb5dfaf84d33bfa
katherin nahomy <molinakatherin40>
2025-08-13 09:36:31 -0700

    Agregue Funciones(getNombre,getSalud) y Metodos(mostrarEstado) ademas del constructor

--- src/main/java/net/perlasjp/juego/Jugador.java
+++ src/main/java/net/perlasjp/juego/Jugador.java
@@ -23,4 +23,26 @@ public class Jugador {
         return salud > 0;
     }
 
+    public Jugador(String n) {
+        nombre = n;
+        salud = 100;
+        fuerza = 10;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public void mostrarEstado() {
+        System.out.println(" Jugador: " + nombre);
+        System.out.println(" Saludo: " + salud);
+        System.out.println(" Ataque: " + fuerza);
+    }
+
 }
